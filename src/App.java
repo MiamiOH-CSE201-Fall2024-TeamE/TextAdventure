0d0d30579b2b6a97815b5556edaf3e3d91941195
TenorTheHusky <programmer2514@gmail.com>
2024-09-04 10:52:00 -0400

    Create Java project

---
+++ src/App.java
@@ -0,0 +1,15 @@
+/**
+ * Main App class, contains the main() method.
+ * 
+ * @author Benjamin Pryor, Brianna Vondra, James Squires,
+ *         Owen Ensley, Ryan Jaruseski, Trevor Coppess
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static boolean shouldReturnTrue() {
+        return true;
+    }
+}
---
+++ test/AppTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+/**
+ * Contains the unit tests to ensure the app functions.
+ * 
+ * @author Benjamin Pryor
+ */
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(App.shouldReturnTrue());
+    }
+
+}
