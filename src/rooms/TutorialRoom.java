2d574bc84b245763b354271ccf1d9103279c36a9
Benjamin Pryor <programmer2514@gmail.com>
2024-10-25 10:45:44 -0400

    Revert TutorialRoom.java to commit 202d97a

--- src/rooms/TutorialRoom.java
+++ src/rooms/TutorialRoom.java
@@ -1,26 +1,17 @@
 package rooms;
 
-import game.Inventory;
-
 /**
  * This is the class for the tutorial room
  * 
  * Coding trail:
- *   10/23/2024 created class stub - Owen
+ *   10/23/2024 - Created class stub (Owen Ensley)
  * 
  * @version 1.0.0
  */
 public class TutorialRoom extends Room {  // TODO: Javadocs
-    private Inventory inventory;
-    private boolean isLocked;
 
     TutorialRoom() {}  // TODO
 
-    public Inventory getInventory() { return inventory; }
-    public boolean isLocked() { return isLocked; }
-    public void lock() { isLocked = true; }
-    public void unlock() { isLocked = false; }
-    public String buildDescription(
-        return "null";
-    );  // TODO
+    @Override
+    public String toString() { return null; }  // TODO
 }
