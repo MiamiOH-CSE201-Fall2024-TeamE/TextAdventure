1237978242d4060d73353d98b99e9f02c7ceea82
OwenEnsley <ensleyoc@miamioh.edu>
2024-11-01 10:10:09 -0400

    Updated Lab class
    I added an ingredients array and checkCorrect method stub

--- src/rooms/Lab.java
+++ src/rooms/Lab.java
@@ -5,11 +5,14 @@ package rooms;
  * 
  * Coding trail:
  *   10/30/2024 - Created class stub (Owen Ensley)
+ *   11/1/2024  - added ingredients array and checkCorrect method stub
  * 
  * @version 1.0.0
  */
 public class Lab extends Room {  // TODO: Javadocs
 
+    private String[] ingredients = new String[3];
+    
     Lab() {}  // TODO
 
     @Override
@@ -19,6 +22,8 @@ public class Lab extends Room {  // TODO: Javadocs
     public String toString() { return null; }  // TODO
 
     public void spawnScientist() {} // TODO
+
+    public boolean checkCorrect(){  return true;  } //TODO
 
 
 }
