fd73e10b3c7fc5362a13386a6e59b214f9c3aa1f
coppestj <coppestj@miamioh.edu>
2024-11-02 15:54:17 -0400

    Update Tutorial.java

--- src/rooms/Tutorial.java
+++ src/rooms/Tutorial.java
@@ -13,23 +13,22 @@ import items.*;
 public class Tutorial extends Room {  // TODO: Javadocs
 
     public static final String NAME = "tutorial";
+    private Player player;
 
-    public Tutorial(boolean isLocked, Inventory inventory) {
+    public Tutorial(Player player, boolean isLocked, Inventory inventory) {
         super(isLocked, inventory);
+        this.player = player;
     }
 
-    public Tutorial() {
-        this(false, new Inventory());
+    public Tutorial(Player player) {
+        this(player, false, new Inventory());
         getInventory().add(new Item("Rock", "A rock you can use to distract the zombies", 3, 1, true, true));
         getInventory().add(new Item("Car", "A car you could throw a rock at to distract the zombies", 1, 0, false, false));
     }  // TODO
 
     @Override
     public boolean use(String toUse, String useOn) { 
-
-        // This is a work in progress
-
-        if (player.getInventory().get(toUse) != null) {
+        if (player.getInventory().get(toUse) != null && toUse.strip().toLowerCase() == "rock") {
             player.getInventory().get(toUse).setAmount(player.getInventory().get(toUse).getAmount() - 1);
             System.out.println("You threw a " + toUse + " at the " + useOn + ", and distracted the zombies. Get to the manor while they are distracted");
             return true;
@@ -38,11 +37,26 @@ public class Tutorial extends Room {  // TODO: Javadocs
             System.out.println("You don't have a " + toUse + " in your inventory.");
             return false;
         }
-    }  // TODO
+    }
 
     @Override
-    public String getDescription() { return null; }  // TODO
+    public String getDescription() {
+        // String outString = "The player " + player.getName() + " is in the Tutorial room. ";
+        
+        // if (getInventory().isEmpty()) {
+        //     outString += "The room is empty.";
+        // } else {
+        //     outString += "You see the following items: ";
+        //     for (Item item : getInventory()) {
+        //         outString += item.getName() + " - " + item.getDescription() + ". ";
+        //     }
+        // }
+        // return outString;
+        return null;
+    }
 
     @Override
-    public String toString() { return NAME; }
+    public String toString() {
+        return NAME;
+    }
 }
