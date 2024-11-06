1400967f1871775648ca003040ffd439aff63e7d
OwenEnsley <ensleyoc@miamioh.edu>
2024-11-06 10:57:09 -0500

    Created String classes for the other rooms
    Bedroom, Lab, Kitchen, Foyer and Cellar all have String classes now

---
+++ src/ui/strings/rooms/Bedroom.java
@@ -0,0 +1,15 @@
+package ui.strings.rooms;
+
+/**
+ * Strings for the rooms.Bedroom class to use.
+ * 
+ * @version 1.0
+ */
+public class Bedroom {
+
+    /**
+     * Prevent instantiating static class.
+     */
+    private Bedroom() {}
+
+}
