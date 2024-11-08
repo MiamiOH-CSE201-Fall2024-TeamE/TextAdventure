1473554d880d7ff480b0629587652feb666aeb2d
coppestj <coppestj@miamioh.edu>
2024-11-08 10:36:08 -0500

    Update Foyer.java

--- src/ui/strings/rooms/Foyer.java
+++ src/ui/strings/rooms/Foyer.java
@@ -13,11 +13,46 @@ public class Foyer {
     private Foyer() {}
 
     /**
+     * The name string for the 'button' item.
+     */
+    public static final String BUTTON
+        = "button";
+
+    /**
+     * The name string for the 'fireplace' item.
+     */
+    public static final String FIREPLACE
+        = "fireplace";
+    
+    /**
+     * The name string for the 'painting' item.
+     */
+    public static final String PAINTING
+        = "painting";
+
+    /**
+     * The name string for the 'board' item.
+     */
+    public static final String BOARD
+        = "board";
+
+    /**
+     * The name string for the 'door' item.
+     */
+    public static final String DOOR
+        = "door";
+    
+    /**
      * The description string for the 'button' item.
      */
     public static final String DESC_BUTTON
         = "There is a button above the fireplace that is labeled with a fire symbol."
         + "It's a mechanical button that is connected to a fire starter";
+
+    public static final String DESC_FIREPLACE = ""; // TODO
+    public static final String DESC_PAINTING = ""; // TODO
+    public static final String DESC_BOARD = ""; // TODO
+    public static final String DESC_DOOR = ""; // TODO
 
     /**
      * The action string for using a board on the door.
