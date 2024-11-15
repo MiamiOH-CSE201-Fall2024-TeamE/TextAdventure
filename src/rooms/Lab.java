e2e475fe96eb4de5da35a0f43ac65b8f677c4079
OwenEnsley <ensleyoc@miamioh.edu>
2024-11-15 10:59:47 -0500

    Implemented Lab Class
    To my knowledge the Lab class is fully implemented. The actual Strings/description id not done yet but it should function

--- src/rooms/Lab.java
+++ src/rooms/Lab.java
@@ -5,9 +5,11 @@ import static game.StateManager.*;
 
 import static ui.strings.rooms.Lab.*;
 import static ui.strings.rooms.Kitchen.KNIFE;
+import static app.App.stateManager;
 
 import game.Input;
 import items.*;
+
 
 /**
  * This is the class for the Lab Room.
@@ -41,6 +43,14 @@ public class Lab extends Room {  // TODO: Javadocs
      */
     private int ending;
 
+    /*
+     * Boolean determining if the scientist is alive
+     */
+    private boolean aliveScientist;
+
+
+    
+
     /**
      * Instantiates a new room with a specified inventory and lock status.
      *
@@ -56,15 +66,15 @@ public class Lab extends Room {  // TODO: Javadocs
      */
     public Lab() { this(true, new Inventory(), false);
 
-        getInventory().add(new Item(ITEM1, DESC_ITEM1, 1, 1, true, true));
-        getInventory().add(new Item(ITEM2, DESC_ITEM2, 1, 1, true, true));
-        getInventory().add(new Item(ITEM3, DESC_ITEM3, 1, 1, true, true));
-        getInventory().add(new Item(ITEM4, DESC_ITEM4, 1, 1, true, true));
-        getInventory().add(new Item(ITEM5, DESC_ITEM5, 1, 1, true, true));
-        getInventory().add(new Item(ITEM6, DESC_ITEM6, 1, 1, true, true));
-        getInventory().add(new Item(ITEM7, DESC_ITEM7, 1, 1, true, true));
-        getInventory().add(new Item(ITEM8, DESC_ITEM8, 1, 1, true, true));
-        getInventory().add(new Item(CURE_MACHINE, DESC_CURE_MACHINE, 1, 1, false, false));
+        getInventory().add(new Item(ITEM1, DESC_ITEM1, 6, 0, true, true));
+        getInventory().add(new Item(ITEM2, DESC_ITEM2, 6, 0, true, true));
+        getInventory().add(new Item(ITEM3, DESC_ITEM3, 6, 0, true, true));
+        getInventory().add(new Item(ITEM4, DESC_ITEM4, 6, 0, true, true));
+        getInventory().add(new Item(ITEM5, DESC_ITEM5, 6, 0, true, true));
+        getInventory().add(new Item(ITEM6, DESC_ITEM6, 6, 0, true, true));
+        getInventory().add(new Item(ITEM7, DESC_ITEM7, 6, 0, true, true));
+        getInventory().add(new Item(ITEM8, DESC_ITEM8, 6, 0, true, true));
+        getInventory().add(new Item(CURE_MACHINE, DESC_CURE_MACHINE, 1, 0, false, false));
 
         ingredients = new Item[3];
         correctIngredients = new Item[] {
@@ -75,6 +85,7 @@ public class Lab extends Room {  // TODO: Javadocs
 
         size = 0;
         ending = END_NONE;
+        aliveScientist = true;
     }  // TODO
 
     @Override
@@ -102,13 +113,16 @@ public class Lab extends Room {  // TODO: Javadocs
                 ingredients[size] = getItem(toUse);
                 size++;
                 removeFromInventory(toUse);
-                checkCorrect();
+                if(size == ingredients.length - 1){
+                    machineFilled();
+                }
                     break;
 
                 default:
                     break;
             }
             return true;
+
         }
 
         // Default case
@@ -118,6 +132,9 @@ public class Lab extends Room {  // TODO: Javadocs
     @Override
     public void pickup(String toPickUp) {}  // TODO
 
+    /*
+     * Returns a description of the room
+     */
     @Override
     public String getDescription() { return null; }  // TODO
 
@@ -125,21 +142,48 @@ public class Lab extends Room {  // TODO: Javadocs
     public String toString() {
         return NAME;
     }
+    
 
+    /*
+     * Handles events once the machine is filled. Will check if the solution is correct
+     *  and also runs the Scientist Encounter Method if the scientist is alive and the solution is wrong
+     */
+    public void machineFilled(){
+        System.out.println(RUNNING_MACHINE);
+        if (checkCorrect()){
+            if (aliveScientist){
+                ending = END_1ST_TRY;
+                stateManager.quitGame();
+            } else {
+                ending = END_2ND_TRY;
+                stateManager.quitGame();
+            }
+        } else {
+            if(aliveScientist){
+                runScientistEncounter();
+            } else {
+                ending = END_FAIL;
+                stateManager.getPlayer().kill(true);
+            }
+        }
+    }
+    
     /**
      * Handles player interaction when the scientist spawns.
      */
-    public void spawnScientist() {
-        getInventory().add(new Item(SCIENTIST, DESC_SCIENTIST, 1, 1, true, true));
+    public void runScientistEncounter() {  
         System.out.println(SCIENTIST_APPEARS);
         if (Input.getConfirmation(FIGHT_PROMPT)) {
             if (getItem(KNIFE) != null) {
-
+                System.out.println(KILL_SCIENTIST);
+                size = 0;
             } else {
                 ending = END_SCI_DIE;
+                stateManager.getPlayer().kill(false);
             }
         } else {
             ending = END_SCI_RUN;
+            stateManager.quitGame();
         }
     }  // TODO
 
@@ -156,10 +200,8 @@ public class Lab extends Room {  // TODO: Javadocs
                     return false;
                 }
             }
-
             return true;
         }
-
         return false;
     }
 
